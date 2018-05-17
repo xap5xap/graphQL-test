a398653972dc6b89c6fc7a30165137fd6c7dcd66
Xavier Perez <xap5xap@users.noreply.github.com>
2018-05-17 16:32:29 -0500

    components

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://xap:xap@ds127490.mlab.com:27490/lyricaldbxap';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
